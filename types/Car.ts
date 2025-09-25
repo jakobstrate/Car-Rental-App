b4af253a84431ae00ec54e80df699347634704dc
victorbp <victor123456bp@gmail.com>
2025-09-25 09:34:01 +0200

    OCD, just changed name of available variable, and ordering of type variables

--- types/Car.ts
+++ types/Car.ts
@@ -1,17 +1,17 @@
 import { ImageSourcePropType } from "react-native";
 
 export type Car = {
+  brand: string;
   modelName: string;
   modelYear: number;
-  brand: string;
-  carType: string;
   rentPerHour: number;
-  transmission: string;
-  fuelType: string;
-  mileage: string;
   image: ImageSourcePropType;
   color: string;
   description: string;
+  carType: string;
+  transmission: string;
+  fuelType: string;
+  mileage: string;
   numberOfSeats: number;
-  available: boolean;
+  isAvailable: boolean;
 };
