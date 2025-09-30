9bb8fa8de115d5c0166f9422f7dd569c5c90b935
Mads Rurup Nielsen <madsr23@student.sdu.dk>
2025-09-30 15:00:35 +0200

    Added functional api and database

---
+++ api.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const db = require("./db");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/cars", (req, res) => {
+  db.all("SELECT * FROM cars", [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({ cars: rows });
+  });
+});
+ 
+app.post("/cars", (req, res) => {
+  const {
+  brand,
+  modelName,
+  modelYear,
+  rentPerHour,
+  image,
+  color,
+  description,
+  carType,
+  transmission,
+  fuelType,
+  mileage,
+  numberOfSeats,
+  extras,
+  isAvailable,
+} = req.body;
+  db.run(
+    `INSERT INTO cars (brand,
+  modelName,
+  modelYear,
+  rentPerHour,
+  image,
+  color,
+  description,
+  carType,
+  transmission,
+  fuelType,
+  mileage,
+  numberOfSeats,
+  extras,
+  isAvailable) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    [
+  brand,
+  modelName,
+  modelYear,
+  rentPerHour,
+  image,
+  color,
+  description,
+  carType,
+  transmission,
+  fuelType,
+  mileage,
+  numberOfSeats,
+  extras,
+  isAvailable,
+],
+    function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({ id: this.lastID,
+  brand,
+  modelName,
+  modelYear,
+  rentPerHour,
+  image,
+  color,
+  description,
+  carType,
+  transmission,
+  fuelType,
+  mileage,
+  numberOfSeats,
+  extras,
+  isAvailable,
+ });
+    }
+  );
+});
+
+// app.get("/users/:id", (req, res) => {
+//   db.get("SELECT * FROM users WHERE id = ?", [req.params.id], (err, row) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+//     if (!row) {
+//       res.status(404).json({ error: "User not found" });
+//       return;
+//     }
+//     res.json(row);
+//   });
+// });
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
